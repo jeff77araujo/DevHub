a23712e71b95d002568015c844fac94aa5f0ce0e
Jeff Araujo <jefferson_lord@yahoo.com.br>
2026-02-21 16:42:56 -0300

    Initial Commit

---
+++ DevHubTests/DevHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevHubTests.swift
+//  DevHubTests
+//
+//  Created by Jefferson Oliveira de Araujo on 21/02/26.
+//
+
+import Testing
+@testable import DevHub
+
+struct DevHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
